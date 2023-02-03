2578690b97a73405d6ae1bf3af29733116788a2e
Mika Stadelmann <mika.stadelmann@tradebyte.com>
2023-02-03 21:57:30 +0100

    collide first steps

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@export var speed = 50
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
@@ -8,4 +8,4 @@ func get_input():
 
 func _physics_process(delta):
 	get_input()
-	move_and_slide()
+	move_and_collide(velocity)
