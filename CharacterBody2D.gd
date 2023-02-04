2c43652367e6ae548001c74a1bbf1a72e290c7d3
Mika Stadelmann <mika.stadelmann@tradebyte.com>
2023-02-04 09:43:36 +0100

    all collision areas

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed = 50
+@export var speed = 400
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
@@ -8,4 +8,4 @@ func get_input():
 
 func _physics_process(delta):
 	get_input()
-	move_and_collide(velocity)
+	move_and_slide()
