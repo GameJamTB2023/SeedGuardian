973543b6bd9d971e2585063c14700d715fd4b52c
pcran@tb-l211 <philipp.cran@tradebyte.com>
2023-02-04 14:54:06 +0100

    add: gameState;

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -5,29 +5,35 @@ extends CharacterBody2D
 
 @export var bullet_scene: PackedScene
 var bullet_spawn: Marker2D
+var game_stats
 
 
 
 func _process(delta):
-	look_at(get_global_mouse_position())
+	if game_stats.game_state_running == true:
+		look_at(get_global_mouse_position())
 	
-	if Input.is_action_just_pressed("fire"):
-		var bullet_instance = bullet_scene.instantiate()
-		bullet_instance.position = bullet_spawn.get_global_position()
-		bullet_instance.rotation_degrees = rotation_degrees
-		bullet_instance.direction = Vector2(1.0,0.0).rotated(rotation).normalized()
-		get_tree().get_root().call_deferred("add_child", bullet_instance)
+		if Input.is_action_just_pressed("fire"):
+			var bullet_instance = bullet_scene.instantiate()
+			bullet_instance.position = bullet_spawn.get_global_position()
+			bullet_instance.rotation_degrees = rotation_degrees
+			bullet_instance.direction = Vector2(1.0,0.0).rotated(rotation).normalized()
+			get_tree().get_root().call_deferred("add_child", bullet_instance)
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 
 func _physics_process(delta):
-	get_input()
-	move_and_slide()
+	if game_stats.game_state_running == true:
+		get_input()
+		move_and_slide()
 	
 func _ready():
 	bullet_spawn = $BulletSpawn
+	game_stats = get_node("/root/game_stats")
+	
+	
 
 
 
--- scripts/main.gd
+++ scripts/main.gd
@@ -3,10 +3,11 @@ extends Node
 @export var mob_scene: PackedScene
 var score: int
 
+var game_stats
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	game_stats = get_node("/root/game_stats")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -14,12 +15,13 @@ func _process(delta):
 
 
 func new_game():
+	game_stats.game_state_running = true
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
 	get_tree().call_group("mobs", "queue_free")
-
 	
 func end_game():
+	game_stats.game_state_running = false
 	$HUD.show_game_over()
 	
 func _on_ScoreTimer_timeout():
