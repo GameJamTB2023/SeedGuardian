93541844040a78ac09a9444136ad2f9f3822046f
Martin Jainta <martin.jainta@tradebyte.com>
2023-02-04 18:30:07 +0100

    Fix sprite mob spawn

--- scripts/map.gd
+++ scripts/map.gd
@@ -8,20 +8,13 @@ var ammunition: int = 10
 func _ready():
 	var rand = RandomNumberGenerator.new()
 	var screen_size = get_viewport().get_visible_rect().size
+	
 	for i in range(1, 10):
-		
 		var collectible = collectible_spawn.instantiate()
-		
-		
-
-		
-
 		add_child(collectible)
 		var area = $full_map
-		var position = area.rect_position + Vector2(randf() * area.rect_size.x, randf() * area.rect_size.y)
+		var position = area.position + Vector2(randf() * area.size.x, randf() * area.size.y)
 		
-		
-
 	$Collectibles/Collectible3.collectible_picked_up.connect(pickup_collectible)
 
 
